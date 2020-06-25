e333d127a0663e8dee88c43d4ea87c75cba06f1f
version-1 <j.onodera26@gmail.com>
2020-06-25 09:16:41 -0700

    fix func

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -9,11 +9,7 @@
 #include <iostream>
 #include <regex>
 
-int calc(std::string str) {
-    int left = stoi(str.substr(1,1));
-    std::string expr = str.substr(2,1);
-    int right = stoi(str.substr(3,1));
-    
+int calc(std::string expr, int left, int right) {
     if (expr == "+") {
         return left + right;
     }
@@ -27,12 +23,15 @@ int calc(std::string str) {
 int evaluate(std::string str) {
     std::regex re1("[0-9]");
     if (regex_match(str, re1)) {
-        return std::stoi(str);
+        return stoi(str);
     }
     
     std::regex re2("\\([0-9][+*][0-9]\\)");
     if (regex_match(str, re2)) {
-        return calc(str);
+        int left = stoi(str.substr(1,1));
+        std::string expr = str.substr(2,1);
+        int right = stoi(str.substr(3,1));
+        return calc(expr, left, right);
     }
     
     std::regex re3("\\([0-9][+*]\\(.+\\)\\)");
@@ -41,25 +40,29 @@ int evaluate(std::string str) {
         std::string expr = str.substr(2,1);
         std::string right = str.substr(3, (str.length() - 1) - 3);
         int i_right = evaluate(right);
-        return calc("(" + std::to_string(left) + expr + std::to_string(i_right) + ")");
+        return calc(expr, left, i_right);
     }
     
-    std::regex re4("\\(\\(.+\\)[+*][0-9]\\)");
-    if (regex_match(str, re3)) {
-        int left = stoi(str.substr(1,1));
-        std::string expr = str.substr(2,1);
-        std::string right = str.substr(3, (str.length() - 1) - 3);
-        int i_right = evaluate(right);
-        return calc("(" + std::to_string(left) + expr + std::to_string(i_right) + ")");
+    std::regex re4("^\\(\\(.+\\)[+*][0-9]\\)$");
+    if (regex_match(str, re4)) {
+        std::string left = str.substr(1, (str.length() - 1) - 3);
+        int i_left = evaluate(left);
+        std::string expr = str.substr(str.length()-3,1);
+        int right = stoi(str.substr(str.length()-2,1));
+        
+        return calc(expr, i_left, right);
     }
     
-    std::regex re5("\\(\\(.+\\)[+*]\\(.+\\)\\)");
-    if (regex_match(str, re3)) {
-        int left = stoi(str.substr(1,1));
-        std::string expr = str.substr(2,1);
-        std::string right = str.substr(3, (str.length() - 1) - 3);
+    std::regex re5("\\((\\(.+\\))[+*](\\(.+\\))\\)");
+    std::smatch sm;
+    regex_search(str, sm, re5);
+    if (sm.size() >= 3) {
+        std::string left = sm[1];
+        std::string expr = str.substr(1 + left.length(),1);
+        std::string right = sm[2];
+        int i_left = evaluate(left);
         int i_right = evaluate(right);
-        return calc("(" + std::to_string(left) + expr + std::to_string(i_right) + ")");
+        return calc(expr, i_left, i_right);
     }
         
     return -1;
@@ -71,9 +74,11 @@ int main(int argc, const char * argv[]) {
     std::cout << "\n";
     std::cout << evaluate("(2*2)");
     std::cout << "\n";
-    std::cout << evaluate("(1+(2*4))");
+    std::cout << evaluate("(1+(2*5))");
     std::cout << "\n";
-    std::cout << evaluate("((1+3)+((1+2)*5))");
+    std::cout << evaluate("((2*4)+1)");
+    std::cout << "\n";
+    std::cout << evaluate("((3*5)+((1+2)*5))");
     std::cout << "\n";
     return 0;
 }
