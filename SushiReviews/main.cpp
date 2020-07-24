dbba7cb94b6b2632e48de318fc8638e44ce9b0c7
version-1 <j.onodera26@gmail.com>
2020-07-24 07:58:22 -0700

    add SushiReviews

---
+++ SushiReviews/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SushiReviews
+//
+//  Created by Administlator on 2020/07/24.
+//  Copyright © 2020 Administlator. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
