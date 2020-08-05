7b30e4f4e861b12158572a2795477543c0083374
version-1 <j.onodera26@gmail.com>
2020-08-05 07:54:39 -0700

    add MST

---
+++ MST/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MST
+//
+//  Created by Administlator on 2020/08/05.
+//  Copyright © 2020 Administlator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
