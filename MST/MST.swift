1fec2fdd3ebf90c461ee7ed2ef68e3c7473a8145
version-1 <j.onodera26@gmail.com>
2020-08-06 07:07:55 -0700

    some fix

--- MST/MST.swift
+++ MST/MST.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+struct Edge {
+  var v: Int
+  var w: Int
+}
+
+extension Edge : Comparable {
+  static func <(lhs: Edge, rhs: Edge) -> Bool {
+    return lhs.w < rhs.w
+  }
+}
+
+extension Edge : Hashable {}
+
 /// Minimum Spanning Tree Algorithms
 public final class MST {
   
@@ -16,7 +29,25 @@ public final class MST {
   /// - Parameter graph: adjacency list of weighted undirected graph where each edge is stored as Tuple
   /// - Returns: the minimum cost spanning tree
   public func primMST(_ graph: [[(v: Int, w: Int)]]) -> Int {
+    let n = graph.count
+    var edges = [Edge]()
+    var pq = IndexPriorityQueue<Edge>(<)
+    var visited = [Bool](repeating: false, count: n)
+    visited[0] = true
+    for edge in graph[0] {
+        pq.enqueue(Edge(v: edge.v, w: edge.w))
+    }
     
+    while let edge = pq.dequeue() {
+        if !visited[edge.v - 1] {
+            visited[edge.v - 1] = true
+            edges.append(edge)
+            for e in graph[edge.v - 1] {
+                pq.enqueue(Edge(v: e.v, w: e.w))
+            }
+        }
+    }
+    return edges.map { $0.w }.reduce(0, +)
   }
   
   /// Kruskal's MST Algorithm O(ElgE)
@@ -24,6 +55,230 @@ public final class MST {
   /// - Parameter graph: adjacency list of weighted undirected graph where each edge is stored as Tuple
   /// - Returns: the minimum cost spanning tree
   public func kruskalMST(_ graph: [[(v: Int, w: Int)]]) -> Int {
+    let n = graph.count
+    var mstEdges = [(u: Int, v: Int, w: Int)]()
+    var allEdges = [(u: Int, v: Int, w: Int)]()
+    for (u, node) in graph.enumerated() {
+      for edge in node {
+        allEdges.append((u: u, v: edge.v, w: edge.w))
+      }
+    }
+    allEdges.sort { $0.w < $1.w }
     
+    var uf = UF(n)
+    for edge in allEdges {
+        if uf.connected(edge.u, edge.v) { continue }
+        uf.union(edge.u, edge.v)
+        mstEdges.append(edge)
+    }
+    
+    return mstEdges.map { $0.w }.reduce(0, +)
+  }
+}
+
+public struct IndexPriorityQueue<Key: Comparable & Hashable> {
+  /// the array that stores the heap's nodes
+  private(set) var elements = [Key]()
+  /// determines whether this is a max-heap(>) or min-heap(<)
+  private var order: (Key, Key) -> Bool
+  /// dictionary from elements to its heap position(index) in the `elements` array
+  private var indices = [Key: Int]()
+  
+  /// Returns true if this priority queue is empty
+  public var isEmpty: Bool {
+    return elements.isEmpty
+  }
+  
+  /// Returns the number of keys on this priority queue.
+  public var count: Int {
+    return elements.count
+  }
+  
+  /// Returns the min or max key O(1)
+  public var peek: Key? {
+    return elements.first
+  }
+  
+  /// Initializes an empty indexed priority queue with indices
+  /// - Parameter order: order of priority queue (`min <` or `max >`)
+  public init(_ order: @escaping (Key, Key) -> Bool) {
+    self.order = order
+  }
+  
+  /// Returns the index of the given key element. (amortized constant time **O(1)**)
+  /// - Parameter key: key element
+  /// - Returns: the index of the given key element
+  public func index(of key: Key) -> Int? {
+    return indices[key]
+  }
+  
+  /// Is `key` on this priority queue? **O(1)**
+  /// - Parameter key: key element
+  /// - Returns: `true` if key exists on this priority queue; `false` otherwise.
+  public func contains(key: Key) -> Bool {
+    return indices[key] != nil
+  }
+  
+  /// Enqueue key element on this priority queue. **O (log N)**
+  /// - Parameter key: key element
+  public mutating func enqueue(_ key: Key) {
+    guard !contains(key: key) else { return }
+    elements.append(key)
+    indices[key] = elements.count - 1
+    siftUp(from: elements.count - 1)
+  }
+  
+  /// Dequeue the root node on this priority queue. **O(log N)**
+  /// - Returns: the root node if the priority queue is not empty
+  @discardableResult
+  public mutating func dequeue() -> Key? {
+    if isEmpty { return nil }
+    let peek = self.peek!
+    swapAt(0, count - 1)
+    elements.remove(at: count - 1)
+    indices[peek] = nil
+    siftDown(from: 0)
+    return peek
+  }
+  
+  /// Update the old key to the new key value. **O(log N)**
+  /// - Parameters:
+  ///   - oldKey: old key
+  ///   - newKey: new key
+  public mutating func update(oldKey: Key, to newKey: Key) {
+    guard let index = indices[oldKey] else { return }
+    elements[index] = newKey
+    siftDown(from: index)
+    siftUp(from: index)
+  }
+  
+  /// Delete the key at index `i` in this priority queue. **O(log N)**
+  /// - Parameter i: index of the key to delete
+  public mutating func delete(at i: Int) {
+    swapAt(i, count - 1)
+    elements.remove(at: count - 1)
+    indices[elements[i]] = nil
+    siftUp(from: i)
+    siftDown(from: i)
+  }
+  
+  /**
+   Private helper methods
+   */
+  private mutating func siftUp(from i: Int) {
+    var i = i
+    while i > 0 && order(elements[parent(of: i)], elements[i]) {
+      swapAt(parent(of: i), i)
+      i = parent(of: i)
+    }
+  }
+  
+  private mutating func siftDown(from i: Int) {
+    var i = i
+    while left(of: i) < count {
+      var j = left(of: i)
+      if j < count - 1 && order(elements[j], elements[j + 1]) {
+        j += 1
+      }
+      if !order(elements[i], elements[j]) { break }
+      swapAt(i, j)
+      i = j
+    }
+  }
+  
+  private mutating func swapAt(_ i: Int, _ j: Int) {
+    elements.swapAt(i, j)
+    indices[elements[i]] = i
+    indices[elements[j]] = j
+  }
+  
+  @inline(__always) private func parent(of index: Int) -> Int {
+    return (index - 1) / 2
+  }
+  
+  @inline(__always) private func left(of index: Int) -> Int {
+    return 2 * index + 1
+  }
+  
+  @inline(__always) private func right(of index: Int) -> Int {
+    return 2 * index + 2
+  }
+}
+
+
+public struct UF {
+  /// parent[i] = parent of i
+  private var parent: [Int]
+  /// size[i] = number of nodes in tree rooted at i
+  private var size: [Int]
+  /// number of components
+  private(set) var count: Int
+  
+  /// Initializes an empty union-find data structure with **n** elements
+  /// **0** through **n-1**.
+  /// Initially, each elements is in its own set.
+  /// - Parameter n: the number of elements
+  public init(_ n: Int) {
+    self.count = n
+    self.size = [Int](repeating: 1, count: n)
+    self.parent = [Int](repeating: 0, count: n)
+    for i in 0..<n {
+      self.parent[i] = i
+    }
+  }
+  
+  /// Returns the canonical element(root) of the set containing element `p`.
+  /// - Parameter p: an element
+  /// - Returns: the canonical element of the set containing `p`
+  public mutating func find(_ p: Int) -> Int {
+    try! validate(p)
+    var root = p
+    while root != parent[root] { // find the root
+      root = parent[root]
+    }
+    var p = p
+    while p != root {
+      let newp = parent[p]
+      parent[p] = root  // path compression
+      p = newp
+    }
+    return root
+  }
+  
+  /// Returns `true` if the two elements are in the same set.
+  /// - Parameters:
+  ///   - p: one elememt
+  ///   - q: the other element
+  /// - Returns: `true` if `p` and `q` are in the same set; `false` otherwise
+  public mutating func connected(_ p: Int, _ q: Int) -> Bool {
+    return find(p) == find(q)
+  }
+  
+  /// Merges the set containing element `p` with the set containing
+  /// element `q`
+  /// - Parameters:
+  ///   - p: one element
+  ///   - q: the other element
+  public mutating func union(_ p: Int, _ q: Int) {
+    let rootP = find(p)
+    let rootQ = find(q)
+    guard rootP != rootQ else { return } // already connected
+    
+    // make smaller root point to larger one
+    if size[rootP] < size[rootQ] {
+      parent[rootP] = rootQ
+      size[rootQ] += size[rootP]
+    } else {
+      parent[rootQ] = rootP
+      size[rootP] += size[rootQ]
+    }
+    count -= 1
+  }
+  
+  private func validate(_ p: Int) throws {
+    let n = parent.count
+    guard p >= 0 && p < n else {
+      return
+    }
   }
 }
