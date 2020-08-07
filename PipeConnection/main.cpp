783967a22ca291f73a35e7b769afedcaf11a3510
version-1 <j.onodera26@gmail.com>
2020-08-07 07:02:09 -0700

    some fix

--- PipeConnection/main.cpp
+++ PipeConnection/main.cpp
@@ -7,9 +7,90 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+ 
+struct UnionFind {  // The range of node number is u 0 v n-1
+    vector<int> rank, parents;
+    UnionFind() {}
+    UnionFind(int n) {  // make n trees.
+        rank.resize(n, 0);
+        parents.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            makeTree(i);
+        }
+    }
+    void makeTree(int x) {
+        parents[x] = x;  // the parent of x is x
+        rank[x] = 0;
+    }
+    bool isSame(int x, int y) {
+        return findRoot(x) == findRoot(y);
+    }
+    void unite(int x, int y) {
+        x = findRoot(x);
+        y = findRoot(y);
+        if (rank[x] > rank[y]) {
+            parents[y] = x;
+        } else {
+            parents[x] = y;
+            if (rank[x] == rank[y]) {
+                rank[y]++;
+            }
+        }
+    }
+    int findRoot(int x) {
+        if (x != parents[x]) parents[x] = findRoot(parents[x]);
+        return parents[x];
+    }
+};
+ 
+struct Edge {
+    long long u;
+    long long v;
+    long long cost;
+};
+bool comp_e(const Edge &e1, const Edge &e2) { return e1.cost < e2.cost; }
+ 
+/* Kruskal :クラスカル法で minimum spanning tree を求める構造体
+    入力: 辺のvector, 頂点数V
+    最小全域木の重みの総和: sum
+    計算量: O(|E|log|V|)
+*/
+struct Kruskal {
+    UnionFind uft;
+    long long sum;  // 最小全域木の重みの総和
+    vector<Edge> edges;
+    int V;
+    Kruskal(const vector<Edge> &edges_, int V_) : edges(edges_), V(V_) {
+        init();
+    }
+    void init() {
+        sort(edges.begin(), edges.end(), comp_e); // 辺の重みでソート
+        uft = UnionFind(V);
+        sum = 0;
+        for (auto e : edges) {
+            if (!uft.isSame(e.u, e.v)) { // 閉路にならなければ加える
+                uft.unite(e.u, e.v);
+                sum += e.cost;
+            }
+        }
+    }
+};
+ 
+int main() {
+    int N, M, D;
+    cout << "input:\n";
+    cin >> N >> M >> D;
+    vector<Edge> edges(M);
+    for (int i = 0; i < M; i++) {
+        long long s, t, w;
+        cin >> s >> t >> w;
+        Edge e = {s - 1, t -1, w};
+        edges[i] = e;
+    }
+    Kruskal krs(edges, N);
+    cout << krs.sum << endl;
     return 0;
 }
