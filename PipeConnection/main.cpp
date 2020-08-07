78fc9222d58cd8571538e30323948785b8182951
version-1 <j.onodera26@gmail.com>
2020-08-07 08:04:30 -0700

    some fix

--- PipeConnection/main.cpp
+++ PipeConnection/main.cpp
@@ -54,25 +54,20 @@ struct Edge {
 };
 bool comp_e(const Edge &e1, const Edge &e2) { return e1.cost < e2.cost; }
  
-/* Kruskal :クラスカル法で minimum spanning tree を求める構造体
-    入力: 辺のvector, 頂点数V
-    最小全域木の重みの総和: sum
-    計算量: O(|E|log|V|)
-*/
 struct Kruskal {
     UnionFind uft;
-    long long sum;  // 最小全域木の重みの総和
+    long long sum;
     vector<Edge> edges;
     int V;
     Kruskal(const vector<Edge> &edges_, int V_) : edges(edges_), V(V_) {
         init();
     }
     void init() {
-        sort(edges.begin(), edges.end(), comp_e); // 辺の重みでソート
+        sort(edges.begin(), edges.end(), comp_e);
         uft = UnionFind(V);
         sum = 0;
         for (auto e : edges) {
-            if (!uft.isSame(e.u, e.v)) { // 閉路にならなければ加える
+            if (!uft.isSame(e.u, e.v)) {
                 uft.unite(e.u, e.v);
                 sum += e.cost;
             }
